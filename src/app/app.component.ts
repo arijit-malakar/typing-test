23d885951f02081733e81f29a683a0b07a4592b5
Arijit Malakar <arijit.malakar@ametek.com>
2023-01-19 15:15:08 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = faker.lorem.sentence();
+  typedText = '';
+
+  onChangeTypedText(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const parsedValue = target.value;
+    this.typedText = parsedValue;
+    console.log(this.typedText);
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+
+    if (randomLetter === ' ' && enteredLetter !== ' ') {
+      return 'incorrect-empty';
+    }
+
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
